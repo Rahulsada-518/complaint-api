10d874ec1c12111b6074fb67b46d2e9aaff6324e
Rahul Sadafule <rahulsadafule@518gmail.com>
2026-06-13 10:46:41 +0530

    Updated Lambda message

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
 exports.handler = async (event) => {
-
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Complaint API Working"
+            message: "Version 2"
         })
     };
-
-=======
-exports.handler = async (event) => {
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Complaint API Working"
-        })
-    };
-
->>>>>>> 9a788f69b41f27a16739a73a9f0c3efb31f60430
 };
